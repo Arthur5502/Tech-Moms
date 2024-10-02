0ba0e3f69db7b1268975a5cd308d42771894358e
Guilherme Wolf Nogueira <161948290+GuilhermeWolf04@users.noreply.github.com>
2024-10-02 18:48:28 -0300

    Add files via upload

---
+++ src/components/DashboardUsuario.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function DashboardUsuario() {
+  return (
+    <div className="dashboard-usuario">
+      <h1>Dashboard do Usuário</h1>
+      <p>Bem-vinda, mãe freelancer!</p>
+    </div>
+  );
+}
+
+export default DashboardUsuario;
