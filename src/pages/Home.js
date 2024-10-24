843c4c9a60500a4ee630133e9f3dc190013c6283
fcyreno <166737877+fcyreno@users.noreply.github.com>
2024-10-24 19:29:35 -0300

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import HomeContent from '../components/HomeContent';
+import '../styles/Home.css';
+import CommunitySection from '../components/CommunitySection';
+import '../styles/CommunitySection.css';
+import Carousel from '../components/Carousel';
+import Footer from '../components/Footer';
+
+const Home = () => {
+  return (
+    <div className="home-page">
+      <HomeContent/>
+      <Carousel />
+      <CommunitySection />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
