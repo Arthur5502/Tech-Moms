073ba465ffeed33534ecee175c8532692bf0d0fd
Guilherme Wolf Nogueira <161948290+GuilhermeWolf04@users.noreply.github.com>
2024-10-24 03:08:09 -0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,6 @@ import '../styles/Header.css';
 const Header = () => {
   return (
     <header className="header">
-      {/* Aqui deve ir a logo */}
-      <img src=" /* link da imagem da logo */ " alt="Logo" className="logo" />
       <Navbar />
     </header>
   );
