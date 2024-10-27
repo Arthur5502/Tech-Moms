a0c12339827cc12ccbe833d25ebf95af0bd20647
Gabrieelgc2 <gabrielao8@hotmail.com>
2024-10-27 15:54:05 -0300

    Update Services

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,21 +2,21 @@ import React from 'react';
 import {motion} from 'framer-motion';
 import { useState, useEffect, useRef } from 'react';
 import '../styles/Carousel.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUsers, faBookOpen, faStar, faEye, faComments, faAward } from '@fortawesome/free-solid-svg-icons';
+import { FaUsers, FaFolderOpen, FaMedal, FaStar, FaComment, FaEye } from 'react-icons/fa';
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"/>
 const Caroussel = [
     { title: 'Networking e Comunidade', description: 'Uma plataforma para mães freelancers se conectarem, trocarem experiências e colaborarem em projetos.',
-    icon: <FontAwesomeIcon icon={faUsers} />},
+    icon: <FaUsers color="#F5A623" size={23}/>},
     { title: 'Tutoriais e Materiais', description: 'Fornecimento de recursos educativos, como modelos de portfólio e currículo, para ajudar mães iniciantes ou que estão retornando ao mercado.',
-     icon: <FontAwesomeIcon icon={faBookOpen}/> },
+     icon:  <FaFolderOpen color="#F5A623" size={23} /> },
     { title: 'Sistema de Pontos', description: 'Um sistema que recompensa mães por tarefas e projetos completados, aumentando sua visibilidade e nota avaliativa.',
-    icon: <FontAwesomeIcon icon={faStar}/> },
+    icon: <FaStar color="#F5A623" size={23}/> },
     { title: 'Visibilidade de Portfólio', description: 'Oportunidades para mães demonstrarem suas habilidades através de tarefas práticas, facilitando a conexão com clientes.',
-    icon: <FontAwesomeIcon icon={faEye}/> },
+    icon:  <FaEye color="#F5A623" size={23}/> },
     { title: 'Feedback de Empresas', description: 'Implementação de um sistema que fornece feedback detalhado e métricas de desempenho, contribuindo para o sistema de pontos.', 
-    icon: <FontAwesomeIcon icon={faComments}/> },
+    icon:  <FaComment color="#F5A623" size={23}/>},
     { title: 'Reconhecimento Público', description: 'Destaque mensal para as mães mais ativas ou bem-sucedidas, aumentando sua visibilidade no mercado.',
-    icon: <FontAwesomeIcon icon={faAward}/> },
+    icon: <FaMedal color="#F5A623" size={23}/> },
 ];
 const Carousel = () => {
     const carousel = useRef();
