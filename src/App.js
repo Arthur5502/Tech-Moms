368f7136125a9db50f97a114eb44fc2e8fbc6812
Arthur_FCampos <162643192+Arthur5502@users.noreply.github.com>
2024-11-01 19:07:45 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import Navbar from './components/Home/Navbar';
+import Home from './pages/Home';
+import LoginForm from './components/Home/LoginForm';
+import Signup from './components/Home/Signup';
+import './styles/Global.css';
+import Carousel from './components/Home/Carousel';
+import CommunitySection from './components/Home/CommunitySection';
+import About from './components/Home/About';
+import DashBoardUsuario from './pages/DashBoardUsuario';
+
+const Main = () => {
+  const location = useLocation();
+
+  return (
+    <>
+      {location.pathname !== '/dashboardUsuario' && <Navbar />}
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/sobre" element={<About />} />
+        <Route path="/servicos" element={<Carousel />} />
+        <Route path="/comunidade" element={<CommunitySection />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboardUsuario" element={<DashBoardUsuario />} />
+      </Routes>
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <Router>
+      <Main />
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/pages/DashBoardUsuario.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import FreelaData from '../components/Dashboard/FreelaData';
+import Contractors from '../components/Dashboard/ContractorsAndSells';
+import '../styles/Dashboard/DashBoardUsuario.css';
+import NavbarDashboard from '../components/Dashboard/NavbarDashboard';
+
+function DashBoardUsuario() {
+    return (
+        <div className="dashboard-section-sells-contractors">
+            <NavbarDashboard />
+            <FreelaData />
+            <Contractors />
+        </div>
+    );
+}
+
+export default DashBoardUsuario;
