65ac910974283bed529bede7df886cd643138523
Gabrieelgc2 <gabrielao8@hotmail.com>
2024-11-04 12:47:08 -0300

    New update

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -5,11 +5,11 @@ const About = () => (
     <section className="about-section">
         <header className="about-header">
             <h3>Sobre</h3>
-            <h2>a TechMom</h2>
+            <h2>As TechMom</h2>
         </header>
         <div className="about-content">
             <p>
-                <strong>TechMom</strong> é uma plataforma que conecta mães freelancers de tecnologia a empresas inclusivas. As profissionais ganham apoio, visibilidade e oportunidades para crescer na carreira. As empresas acessam talentos diversos e reforçam sua imagem inclusiva. Juntos, construímos um setor de tecnologia mais equilibrado e inclusivo!
+                <strong>A TechMom</strong> é uma plataforma que conecta mães freelancers de tecnologia a empresas inclusivas. As profissionais ganham apoio, visibilidade e oportunidades para crescer na carreira. As empresas acessam talentos diversos e reforçam sua imagem inclusiva. Juntos, construímos um setor de tecnologia mais equilibrado e inclusivo!
             </p>
         </div>
     </section>
