a4970ccbac00ea2ee109075473c1314598f23754
João Lira <166864039+joaosanli@users.noreply.github.com>
2024-11-04 18:58:20 -0300

    Update Workflow.js

--- Tech-Moms-JoaoLira/src/components/Workflow.js
+++ Tech-Moms-JoaoLira/src/components/Workflow.js
@@ -9,44 +9,68 @@ const Workflow = () => {
         <strong>na Techmom Trabalham</strong>
       </h1>
       <div className="top-line"></div>
-      <div className="workflow-steps">
-        <div className="step">
-          <h2>
-            <span className="step-number">#1</span> Matching do Projeto
-          </h2>
-          <p>Texto Exemplo Texto Exemplo Texto Exemplo</p>
+
+      <div className="timeline">
+        <div className="step-container">
+          <div className="step">
+            <h2>
+              <span className="step-number">#1</span> Matching do Projeto
+            </h2>
+            <p>Texto Exemplo Texto Exemplo Texto Exemplo</p>
+          </div>
+          <div className="divider-line"></div>
         </div>
-        <div className="step">
-          <h2>
-            <span className="step-number">#3</span> Desenvolvimento
-          </h2>
-          <p>Texto Exemplo Texto Exemplo Texto Exemplo</p>
+
+        <div className="step-container">
+          <div className="step">
+            <h2>
+              <span className="step-number">#2</span> Comunicação Cliente
+            </h2>
+            <p>Texto Exemplo Texto Exemplo Texto Exemplo</p>
+          </div>
+          <div className="divider-line"></div>
         </div>
-        <div className="step">
-          <h2>
-            <span className="step-number">#5</span> Entrega do Projeto
-          </h2>
-          <p>Texto Exemplo Texto Exemplo Texto Exemplo</p>
+
+        <div className="step-container">
+          <div className="step">
+            <h2>
+              <span className="step-number">#3</span> Desenvolvimento
+            </h2>
+            <p>Texto Exemplo Texto Exemplo Texto Exemplo</p>
+          </div>
+          <div className="divider-line"></div>
         </div>
-        <div className="step">
-          <h2>
-            <span className="step-number">#2</span> Comunicação Cliente
-          </h2>
-          <p>Texto Exemplo Texto Exemplo Texto Exemplo</p>
+
+        <div className="step-container">
+          <div className="step">
+            <h2>
+              <span className="step-number">#4</span> Revisão da Entrega
+            </h2>
+            <p>Texto Exemplo Texto Exemplo Texto Exemplo</p>
+          </div>
+          <div className="divider-line"></div>
         </div>
-        <div className="step">
-          <h2>
-            <span className="step-number">#4</span> Revisão da Entrega
-          </h2>
-          <p>Texto Exemplo Texto Exemplo Texto Exemplo</p>
+
+        <div className="step-container">
+          <div className="step">
+            <h2>
+              <span className="step-number">#5</span> Entrega do Projeto
+            </h2>
+            <p>Texto Exemplo Texto Exemplo Texto Exemplo</p>
+          </div>
+          <div className="divider-line"></div>
         </div>
-        <div className="step">
-          <h2>
-            <span className="step-number">#6</span> Feedback e Avaliação
-          </h2>
-          <p>Texto Exemplo Texto Exemplo Texto Exemplo</p>
+
+        <div className="step-container">
+          <div className="step">
+            <h2>
+              <span className="step-number">#6</span> Feedback e Avaliação
+            </h2>
+            <p>Texto Exemplo Texto Exemplo Texto Exemplo</p>
+          </div>
         </div>
       </div>
+
       <div className="trophy-icon">
         <img src="../assets/trofeu.png" alt="Troféu" />
       </div>
@@ -55,3 +79,4 @@ const Workflow = () => {
 };
 
 export default Workflow;
+
