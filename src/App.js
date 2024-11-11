940bd650dbed097432e820bff5e5c50889d3e32b
Arthur_FCampos <162643192+Arthur5502@users.noreply.github.com>
2024-11-11 20:25:44 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Home/Navbar';
+import Home from './pages/Home';
+import LoginForm from './components/Home/LoginForm';
+import Signup from './components/Home/Signup';
+import './styles/Global.css';
+import Carousel from './components/Home/Carousel';
+import CommunitySection from './components/Home/CommunitySection';
+import About from './components/Home/About';
+import DashBoardUsuario from './pages/DashBoardUsuario';
+import DashBoardEmpresa from './pages/DashBoardEmpresa';
+import NavbarUsuario from './components/DashboardUsuario/NavbarUsuario';
+import NavbarBusiness from './components/DashboardEmpresa/NavbarBusiness';
+import UserProfile from './components/DashboardUsuario/UserProfile';
+
+const Main = () => {
+  return (
+    <>
+       <Routes>
+        <Route path="/dashboardUsuario" element={<NavbarUsuario />} />
+        <Route path="/userProfile" element={<NavbarUsuario />} />
+        <Route path="/dashboardEmpresa" element={<NavbarBusiness />} />
+        <Route path="*" element={<Navbar />} />
+      </Routes>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/sobre" element={<About />} />
+        <Route path="/servicos" element={<Carousel />} />
+        <Route path="/comunidade" element={<CommunitySection />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboardUsuario" element={<DashBoardUsuario />} />
+        <Route path="/dashboardEmpresa" element={<DashBoardEmpresa />} />
+        <Route path="/userProfile" element={<UserProfile />} />
+      </Routes>
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <Router>
+      <Main />
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/components/DashboardUsuario/SidebarUsuario.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { MdSettings, MdLogout, MdShoppingCart, MdEmail, MdFavorite, MdDashboard, MdSchool, MdHome } from 'react-icons/md';
+import { Link, useLocation } from 'react-router-dom';
+import '../../styles/DashboardUsuario/SidebarUsuario.css';
+
+function Sidebar({ isExpanded }) {
+  const location = useLocation();
+
+  return (
+    <nav className={`sidebar ${isExpanded ? 'expanded' : ''}`}>
+      <ul>
+        <li className={location.pathname === '/DashboardUsuario' ? 'active' : ''}>
+          <Link to="/DashboardUsuario">
+            <MdDashboard className="react-icon"/>
+            <span className={`sidebar-text ${!isExpanded && 'hidden'}`}>Dashboard</span>
+          </Link>
+        </li>
+        <li>
+          <a href="#">
+            <MdHome className="react-icon"/>
+            <span className={`sidebar-text ${!isExpanded && 'hidden'}`}>Feed</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <MdFavorite className="react-icon"/>
+            <span className={`sidebar-text ${!isExpanded && 'hidden'}`}>Favoritos</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <MdEmail className="react-icon"/>
+            <span className={`sidebar-text ${!isExpanded && 'hidden'}`}>Chats</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <MdShoppingCart className="react-icon"/>
+            <span className={`sidebar-text ${!isExpanded && 'hidden'}`}>Pedidos</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <MdSchool className="react-icon"/>
+            <span className={`sidebar-text ${!isExpanded && 'hidden'}`}>Cursos</span>
+          </a>
+        </li>
+      </ul>
+      <div className="bottom-links">
+        <a href="#">
+          <MdSettings className="react-icon"/>
+          <span className={`sidebar-text ${!isExpanded && 'hidden'}`}>Configurações</span>
+        </a>
+        <a href="/">
+          <MdLogout className="react-icon"/>
+          <span className={`sidebar-text ${!isExpanded && 'hidden'}`}>Logout</span>
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default Sidebar;
