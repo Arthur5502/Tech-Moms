17a78f25113e747694b4b8ba44a71ba15c2e2ff8
Guilherme Wolf <gwn@cesar.school>
2024-11-23 22:23:49 -0300

    user dashboard responsivity

--- src/components/DashboardUsuario/ProjectTable.js
+++ src/components/DashboardUsuario/ProjectTable.js
@@ -10,9 +10,9 @@ const ProjectTable = () => {
   ];
 
   const projects = [
-    { name: "CESAR", project: "VoltAI", date: "12/09/2024 - 12:53", tech: "FullStack", revenue: "R$342,95", status: "Concluido" },
-    { name: "Apple", project: "BioFuture", date: "12/09/2024 - 12:53", tech: "FullStack", revenue: "R$100,95", status: "Cancelado" },
-    { name: "Accenture", project: "TechMoms", date: "12/09/2024 - 12:53", tech: "FullStack", revenue: "R$34.295", status: "Andamento" },
+    { name: "CESAR", project: "VoltAI", date: "12/09/2024 - 12:53", revenue: "R$342,95", status: "Concluido" },
+    { name: "Apple", project: "BioFuture", date: "12/09/2024 - 12:53", revenue: "R$100,95", status: "Cancelado" },
+    { name: "Accenture", project: "TechMoms", date: "12/09/2024 - 12:53", revenue: "R$34.295", status: "Andamento" },
   ];
 
   return (
@@ -36,7 +36,6 @@ const ProjectTable = () => {
         <div>CONTRATANTE</div>
         <div>PROJETO</div>
         <div>DATA - HORA</div>
-        <div>TECNOLOGIA</div>
         <div>RENDA</div>
         <div>STATUS</div>
       </div>
@@ -52,7 +51,6 @@ const ProjectTable = () => {
             </div>
             <div className="cell">{project.project}</div>
             <div className="cell">{project.date}</div>
-            <div className="cell">{project.tech}</div>
             <div className="cell">{project.revenue}</div>
             <div className="cell">
               <span className={`status ${project.status.toLowerCase()}`}>{project.status}</span>
