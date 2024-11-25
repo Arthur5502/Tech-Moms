7f1a8cf8fe1560fad8e36dfdb3f4f335de588add
Arthur5502 <afcp@cesar.school>
2024-11-25 13:19:39 -0300

    updates

--- src/components/Home/Signup.js
+++ src/components/Home/Signup.js
@@ -99,203 +99,205 @@ const Signup = () => {
   };
 
   return (
-    <div className="signup-container">
-      <main className="signup-form">
-        <div className="signup-header">
-          <div className="signup-text">
-            <h2>Bem-vinda(o)!</h2>
-            <h3>Crie sua Conta</h3>
+    <div className="signup-page">
+      <div className="signup-container">
+        <main className="signup-form">
+          <div className="signup-header">
+            <div className="signup-text">
+              <h2>Bem-vinda(o)!</h2>
+              <h3>Crie sua Conta</h3>
+            </div>
+
+            <div className="role-buttons">
+              <button 
+                className={`freela-button ${!isCompany ? 'active' : ''}`} 
+                onClick={() => setIsCompany(false)}
+              >
+                Sou Freela
+              </button>
+              <button 
+                className={`hire-button ${isCompany ? 'active' : ''}`} 
+                onClick={() => setIsCompany(true)}
+              >
+                Quero Contratar
+              </button>
+            </div>
           </div>
 
-          <div className="role-buttons">
-            <button 
-              className={`freela-button ${!isCompany ? 'active' : ''}`} 
-              onClick={() => setIsCompany(false)}
-            >
-              Sou Freela
-            </button>
-            <button 
-              className={`hire-button ${isCompany ? 'active' : ''}`} 
-              onClick={() => setIsCompany(true)}
-            >
-              Quero Contratar
-            </button>
-          </div>
-        </div>
+          <form className="form" onSubmit={handleSubmit}>
+            {isCompany ? (
+              <>
+                <div className="form-row">
+                  <div className="form-group">
+                    <label>Nome da Empresa:</label>
+                    <input 
+                      type="text" 
+                      name="companyName"
+                      placeholder="Nome da empresa" 
+                      value={formData.companyName}
+                      onChange={handleChange}
+                      required 
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label>Email:</label>
+                    <input 
+                      type="email" 
+                      name="email"
+                      placeholder="exemplo@gmail.com" 
+                      value={formData.email}
+                      onChange={handleChange}
+                      required 
+                    />
+                  </div>
+                </div>
 
-        <form className="form" onSubmit={handleSubmit}>
-          {isCompany ? (
-            <>
-              <div className="form-row">
-                <div className="form-group">
-                  <label>Nome da Empresa:</label>
-                  <input 
-                    type="text" 
-                    name="companyName"
-                    placeholder="Nome da empresa" 
-                    value={formData.companyName}
-                    onChange={handleChange}
-                    required 
-                  />
+                <div className="form-row">
+                  <div className="form-group">
+                    <label>CNPJ:</label>
+                    <InputMask 
+                      mask= "99.999.999/9999-99"
+                      name="cnpj"
+                      placeholder="00.000.000/0000-00" 
+                      value={formData.cnpj}
+                      onChange={handleChange}
+                      required 
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label>Localização:</label>
+                    <input 
+                      type="text" 
+                      name="location"
+                      placeholder="Endereço ou cidade" 
+                      value={formData.location}
+                      onChange={handleChange}
+                      required 
+                    />
+                  </div>
                 </div>
-                <div className="form-group">
-                  <label>Email:</label>
-                  <input 
-                    type="email" 
-                    name="email"
-                    placeholder="exemplo@gmail.com" 
-                    value={formData.email}
-                    onChange={handleChange}
-                    required 
-                  />
-                </div>
-              </div>
 
-              <div className="form-row">
-                <div className="form-group">
-                  <label>CNPJ:</label>
-                  <InputMask 
-                    mask= "99.999.999/9999-99"
-                    name="cnpj"
-                    placeholder="00.000.000/0000-00" 
-                    value={formData.cnpj}
-                    onChange={handleChange}
-                    required 
-                  />
+                <div className="form-row">
+                  <div className="form-group">
+                    <label>Senha:</label>
+                    <input 
+                      type="password" 
+                      name="password"
+                      placeholder="*********" 
+                      value={formData.password}
+                      onChange={handleChange}
+                      required 
+                    />
+                  </div>
+                  <div className="form-group input-password">
+                    <label>Confirme sua Senha:</label>
+                    <input 
+                      type="password" 
+                      name="confirmPassword"
+                      placeholder="*********" 
+                      value={formData.confirmPassword}
+                      onChange={handleChange}
+                      required 
+                    />
+                  </div>
                 </div>
-                <div className="form-group">
-                  <label>Localização:</label>
-                  <input 
-                    type="text" 
-                    name="location"
-                    placeholder="Endereço ou cidade" 
-                    value={formData.location}
-                    onChange={handleChange}
-                    required 
-                  />
+              </>
+            ) : (
+              <>
+                <div className="form-row">
+                  <div className="form-group">
+                    <label>Nome:</label>
+                    <input 
+                      type="text" 
+                      name="firstName"
+                      placeholder="Insira seu nome" 
+                      value={formData.firstName}
+                      onChange={handleChange}
+                      required 
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label>Sobrenome:</label>
+                    <input 
+                      type="text" 
+                      name="lastName"
+                      placeholder="Insira seu sobrenome" 
+                      value={formData.lastName}
+                      onChange={handleChange}
+                      required 
+                    />
+                  </div>
                 </div>
-              </div>
 
-              <div className="form-row">
-                <div className="form-group">
-                  <label>Senha:</label>
-                  <input 
-                    type="password" 
-                    name="password"
-                    placeholder="*********" 
-                    value={formData.password}
-                    onChange={handleChange}
-                    required 
-                  />
+                <div className="form-row">
+                  <div className="form-group">
+                    <label>CPF:</label>
+                    <InputMask 
+                      mask= "999.999.999-99"
+                      name="cpf"
+                      placeholder="111.111.111-11" 
+                      value={formData.cpf}
+                      onChange={handleChange}
+                      required 
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label>Data de Aniversário:</label>
+                    <InputMask
+                      mask= "99/99/9999"
+                      name="birthDate"
+                      placeholder="mm/dd/aaaa" 
+                      value={formData.birthDate}
+                      onChange={handleChange}
+                      required 
+                    />
+                  </div>
                 </div>
-                <div className="form-group input-password">
-                  <label>Confirme sua Senha:</label>
-                  <input 
-                    type="password" 
-                    name="confirmPassword"
-                    placeholder="*********" 
-                    value={formData.confirmPassword}
-                    onChange={handleChange}
-                    required 
-                  />
-                </div>
-              </div>
-            </>
-          ) : (
-            <>
-              <div className="form-row">
-                <div className="form-group">
-                  <label>Nome:</label>
-                  <input 
-                    type="text" 
-                    name="firstName"
-                    placeholder="Insira seu nome" 
-                    value={formData.firstName}
-                    onChange={handleChange}
-                    required 
-                  />
-                </div>
-                <div className="form-group">
-                  <label>Sobrenome:</label>
-                  <input 
-                    type="text" 
-                    name="lastName"
-                    placeholder="Insira seu sobrenome" 
-                    value={formData.lastName}
-                    onChange={handleChange}
-                    required 
-                  />
-                </div>
-              </div>
 
-              <div className="form-row">
-                <div className="form-group">
-                  <label>CPF:</label>
-                  <InputMask 
-                    mask= "999.999.999-99"
-                    name="cpf"
-                    placeholder="111.111.111-11" 
-                    value={formData.cpf}
-                    onChange={handleChange}
-                    required 
-                  />
+                <div className="form-row">
+                  <div className="form-group">
+                    <label>Senha:</label>
+                    <input 
+                      type="password" 
+                      name="password"
+                      placeholder="*********" 
+                      value={formData.password}
+                      onChange={handleChange}
+                      required 
+                    />
+                  </div>
+                  <div className="form-group input-password">
+                    <label>Confirme sua Senha:</label>
+                    <input 
+                      type="password" 
+                      name="confirmPassword"
+                      placeholder="*********" 
+                      value={formData.confirmPassword}
+                      onChange={handleChange}
+                      required 
+                    />
+                  </div>
                 </div>
-                <div className="form-group">
-                  <label>Data de Aniversário:</label>
-                  <InputMask
-                    mask= "99/99/9999"
-                    name="birthDate"
-                    placeholder="mm/dd/aaaa" 
-                    value={formData.birthDate}
-                    onChange={handleChange}
-                    required 
-                  />
-                </div>
-              </div>
+              </>
+            )}
+            <div className="terms-container">
+              <input 
+                type="checkbox" 
+                required 
+                onChange={(e) => setTermsAccepted(e.target.checked)} 
+              />
+              <label>
+                Aceito os Termos de Serviço e a Política de Privacidade
+              </label>
+            </div>
+            
+              <button type="submit" className="submit-button"><Link to="/dashboardEmpresa">Criar conta</Link></button>
+            
+          </form>
 
-              <div className="form-row">
-                <div className="form-group">
-                  <label>Senha:</label>
-                  <input 
-                    type="password" 
-                    name="password"
-                    placeholder="*********" 
-                    value={formData.password}
-                    onChange={handleChange}
-                    required 
-                  />
-                </div>
-                <div className="form-group input-password">
-                  <label>Confirme sua Senha:</label>
-                  <input 
-                    type="password" 
-                    name="confirmPassword"
-                    placeholder="*********" 
-                    value={formData.confirmPassword}
-                    onChange={handleChange}
-                    required 
-                  />
-                </div>
-              </div>
-            </>
-          )}
-          <div className="terms-container">
-            <input 
-              type="checkbox" 
-              required 
-              onChange={(e) => setTermsAccepted(e.target.checked)} 
-            />
-            <label>
-              Aceito os Termos de Serviço e a Política de Privacidade
-            </label>
-          </div>
-          
-            <button type="submit" className="submit-button"><Link to="/dashboardEmpresa">Criar conta</Link></button>
-          
-        </form>
-
-        <p>Já tem uma conta? <a href="/login">Login</a></p>
-      </main>
+          <p>Já tem uma conta? <a href="/login">Login</a></p>
+        </main>
+      </div>
     </div>
   );
 };
